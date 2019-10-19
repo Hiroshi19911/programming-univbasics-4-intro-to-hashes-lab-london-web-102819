ff02d88fc1c72e4460250b052244588b83ea2ee1
Hiroshi Sipahioglu <hiroshis91@icloud.com>
2019-10-19 20:50:35 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -3,7 +3,7 @@ def new_hash
 end
 
 def my_hash
-   # return a valid hash with any key/value pair of your choice
+  my hash = {:name => Hiro, :age => 28} # return a valid hash with any key/value pair of your choice
 end
 
 def pioneer
